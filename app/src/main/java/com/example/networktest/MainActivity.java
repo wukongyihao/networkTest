5ea0243a7d14fe96a5a7799fc47ef1a727f7ce10
xiaoming <961775890@qq.com>
2019-05-28 22:46:13 +0800

    9.3.1 5.28

--- app/src/main/java/com/example/networktest/MainActivity.java
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -3,16 +3,26 @@ package com.example.networktest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Xml;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
+import java.net.ResponseCache;
 import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -30,9 +40,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.send_request){
-            sendRequestWithHttpURLConnection();
+//            sendRequestWithHttpURLConnection();
+            sendRequestWithOkHttp();
         }
     }
+
+    private void sendRequestWithOkHttp(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder().url("http://10.0.2.2/get_data.xml").build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    parseXMLWithPull(responseData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void parseXMLWithPull(String xmlData){
+        try{
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+            String id = "";
+            String name = "";
+            String version = "";
+            while (eventType != XmlPullParser.END_DOCUMENT){
+                String nodeName = xmlPullParser.getName();
+                switch (eventType){
+                    case XmlPullParser.START_TAG:{
+                        if ("id".equals(nodeName)){
+                            id = xmlPullParser.nextText();
+                        }else if ("name".equals(nodeName)){
+                            name = xmlPullParser.nextText();
+                        }else if ("version".equals(nodeName)){
+                            version = xmlPullParser.nextText();
+                        }
+                        break;
+                    }
+                    case XmlPullParser.END_TAG:{
+                        if ("app".equals(nodeName)){
+                            Log.d("MainActivity","id is "+ id);
+                            Log.d("MainActivity","name is "+ name);
+                            Log.d("MainActivity","version is "+ version);
+                        }
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
 
     protected void sendRequestWithHttpURLConnection(){
         new Thread(new Runnable() {
@@ -56,8 +126,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             response.append(line);
 //                            Log.d("123123","append line");
                         }
-                    showResponse(response.toString());
-//                    Log.d("123123","show response");
+//                    showResponse(response.toString());
+                    Log.d("123123","show response");
                 }catch (Exception e){
                     e.printStackTrace();
                 }finally {
